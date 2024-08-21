c6c1ae7f97e6172bc9f895b98cf053a2a4eff990
nitverse <nmakwana496@gmail.com>
2024-08-21 17:20:09 +0530

    Initial set-up 😼

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,11 @@
+
+const page = () => {
+  return (
+    <div className="flex text-5xl font-semibold overflow-hidden p-2">
+      page
+      <div className="px-3">page</div>
+    </div>
+  );
+};
+
+export default page;
