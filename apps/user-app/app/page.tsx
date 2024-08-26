a5ea769760d416cbc794fe71375bbbcc8c0c3465
nitverse <nmakwana496@gmail.com>
2024-08-26 23:52:41 +0530

    Testing :)

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -4,8 +4,6 @@ import { authOptions } from "../lib/auth";
 
 export default async function Page() {
   const session = await getServerSession(authOptions);
-
-  
   if (session?.user) {
     redirect("/transfer");
   } else {
